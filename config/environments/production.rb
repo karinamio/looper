2eef3161980ca570ff331107eaee52783a8954fb
Karina Mio <me@karinamio.com>
2014-09-22 00:28:33 -0400

    heroku forcing commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Treebook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.assets.initialize_on_precompile = true
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
